f7c7dc97e0a8915ee0cff6d77e6d0437fc4abe3f
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-08 16:13:41 +0300

    Create ToDo Model

---
+++ ToDo-backend/ToDo-backend/Models/ToDo.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo_backend.Models
+{
+    public class ToDo
+    {
+        public int Id;
+        public string Title { get; set; } = "Example ToDo";
+        public string Description { get; set; } = "ToDo description";
+        public int Priority { get; set; } = 0;
+        public DateTime Due { get; set; } = (DateTime.Now).AddDays(1);
+        public DateTime StartTime { get; set; } = DateTime.Now;
+    }
+}
