d0c0348eb80eea23a6972a077ef5146e0191c025
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-08 16:13:52 +0300

    Create DTO's for Model

---
+++ ToDo-backend/ToDo-backend/Dtos/GetToDoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDo_backend.Dtos
+{
+    public class GetToDoDto
+    {
+        public int Id;
+        public string Title { get; set; } = "Example ToDo";
+        public string Description { get; set; } = "ToDo description";
+        public int Priority { get; set; } = 0;
+        public DateTime Due { get; set; } = (DateTime.Now).AddDays(1);
+        public DateTime StartTime { get; set; } = DateTime.Now;
+    }
+}
