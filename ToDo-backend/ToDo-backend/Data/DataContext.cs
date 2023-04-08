90b450f29a27ddcc97fceb25dc7e74a9a3598d8f
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-08 16:24:51 +0300

    Create DataContext.cs

---
+++ ToDo-backend/ToDo-backend/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace ToDo_backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ToDo> ToDoList { get; set; }
+
+    }
+
+
+}
