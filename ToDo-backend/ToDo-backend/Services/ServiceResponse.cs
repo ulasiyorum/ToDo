dc91b950b8ad1406ff3c11cdaf30041cc6cb4bdb
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-08 16:15:29 +0300

    Create ServiceResponse.cs

---
+++ ToDo-backend/ToDo-backend/Services/ServiceResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDo_backend.Services
+{
+    public class ServiceResponse<T>
+    {
+        T? Data { get; set; }
+        public bool Success { get; set; } = true;
+        public string Message { get; set; } = "Successful";
+    }
+}
