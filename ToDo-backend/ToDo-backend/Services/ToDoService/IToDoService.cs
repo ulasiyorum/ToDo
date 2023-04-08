6714163fcfa936bc4324047ef4b157031d24027e
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-08 16:20:39 +0300

    ToDo service without database and mapper

---
+++ ToDo-backend/ToDo-backend/Services/ToDoService/IToDoService.cs
@@ -0,0 +1,12 @@
+﻿global using ToDo_backend.Dtos;
+
+namespace ToDo_backend.Services.ToDoService
+{
+    public interface IToDoService
+    {
+        Task<ServiceResponse<List<GetToDoDto>>> GetAll();
+        Task<ServiceResponse<List<GetToDoDto>>> Update(UpdateToDoDto updateDto);
+        Task<ServiceResponse<List<GetToDoDto>>> Delete(int id);
+        Task<ServiceResponse<List<GetToDoDto>>> Add(AddToDoDto addDto);
+    }
+}
---
+++ ToDo-backend/ToDo-backend/Services/ToDoService/ToDoService.cs
@@ -0,0 +1,25 @@
+﻿namespace ToDo_backend.Services.ToDoService
+{
+    public class ToDoService : IToDoService
+    {
+        public Task<ServiceResponse<List<GetToDoDto>>> Add(AddToDoDto addDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<GetToDoDto>>> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<GetToDoDto>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<List<GetToDoDto>>> Update(UpdateToDoDto updateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
