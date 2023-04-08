1541cf19fa6c0c0c30de5bb705e1a898305c6fe3
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-08 16:22:19 +0300

    Create AutoMapperProfile.cs

---
+++ ToDo-backend/ToDo-backend/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿global using AutoMapper;
+global using ToDo_backend.Models;
+
+namespace ToDo_backend
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<ToDo, GetToDoDto>();
+            CreateMap<AddToDoDto, ToDo>();
+            CreateMap<UpdateToDoDto, ToDo>();
+        }
+    }
+}
