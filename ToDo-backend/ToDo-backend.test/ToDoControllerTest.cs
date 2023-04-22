400b223a623a944bce6efc201e90d6ac8174e702
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-04-22 20:42:09 +0300

    test GetUsersList_ReturnsUsersToDoItems()

--- ToDo-backend/ToDo-backend.test/ToDoControllerTest.cs
+++ ToDo-backend/ToDo-backend.test/ToDoControllerTest.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using ToDo_backend.Controllers;
 using ToDo_backend.Data;
 using ToDo_backend.Dtos;
+using ToDo_backend.Dtos.UserDto;
 using ToDo_backend.Services;
 using ToDo_backend.Services.ToDoService;
 
@@ -39,7 +40,7 @@ namespace ToDo_backend.test
             var result = await _service.GetAll();
 
             var okResult = (await _controller.GetAll()).Result as OkObjectResult;
-
+            Assert.NotNull(okResult);
             var res = okResult.Value as ServiceResponse<List<GetToDoDto>>;
             Assert.NotNull(res);
             Assert.True(res.Success);
@@ -47,6 +48,28 @@ namespace ToDo_backend.test
             // Assert
             Assert.IsType<ServiceResponse<List<GetToDoDto>>>(result);
             Assert.True(result.Success);
+        }
+
+        [Fact]
+        public async Task GetUsersList_ReturnsUsersToDoItems()
+        {
+            // Arrange
+            int userId = 1;
+
+            // Act
+            var okResult = (await _controller.GetById(userId)).Result as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(okResult);
+            var list = okResult.Value as ServiceResponse<List<GetToDoDto>>;
+            Assert.IsType<ServiceResponse<List<GetToDoDto>>>(list);
+
+            if(list.Data != null && list.Data.Count > 0)
+            {
+                Assert.True(list.Data[0].UserId == userId);
+            }
+            Assert.NotNull(list);
+            Assert.True(list.Success);
         }
     }
 }
